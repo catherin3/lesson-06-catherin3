c886d37c08a91f6ac3b69498ce08d06c5890959d
Lydia <lydia_hamid@rp.edu.sg>
2019-11-20 13:54:12 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * C308 LESSON 06
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ Lesson06quiz.js
@@ -0,0 +1,25 @@
+/**
+ * C308 Lesson 06 Quiz
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const Lesson06quiz = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+export default Lesson06quiz;
